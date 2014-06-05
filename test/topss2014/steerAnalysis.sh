345013a85a815e61c4c109decffbe64ad4bc3865
Pedro Silva <psilva@cern.ch>
2014-06-05 22:41:57 +0200

    first pass

---
+++ test/topss2014/steerAnalysis.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+# full analysis
+#
+
+step=$1
+optim_step=$2
+outdir="${CMSSW_BASE}/src/UserCode/llvv_fwk/test/topss2014"
+indir="/store/cmst3/user/psilva/5311_ntuples"
+cfg="$CMSSW_BASE/src/UserCode/llvv_fwk/test/runAnalysis_cfg.py.templ"
+
+
+#prepare output directories
+mkdir -p ${outdir}/summary/
+
+if [ "$step" == "1" ]; then
+    echo "Submitting sample pre-selection"
+    runLocalAnalysisOverSamples.py -e runTopAnalysis -j ${outdir}/samples.json  -d ${indir} -o ${outdir}/summary/ -c ${cfg} -p "@saveSummaryTree=True";
+    echo "You can find a summary with the selected events @ ${outdir} after all jobs have finished"
+fi
+
+if [ "$step" == "2" ]; then
+    echo "Computing weights"
+    runPlotter --iLumi 19736 --inDir ${outdir}/summary/ --json ${outdir}/samples.json --outFile ${outdir}/plotter.root --forceMerged;
+fi

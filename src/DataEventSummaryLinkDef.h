91841d6cb4580b9c2192310e448a4b2c25ce5a26
Benjamin Stieger <stiegerb@cern.ch>
2014-06-12 17:33:26 +0200

    LxyTreeAnalysis riding piggyback on DataEventSummaryLinkDef for now

--- src/DataEventSummaryLinkDef.h
+++ src/DataEventSummaryLinkDef.h
@@ -1,13 +1,15 @@
 #include "UserCode/llvv_fwk/interface/DataEventSummary.h"
+#include "UserCode/llvv_fwk/interface/LxyTreeAnalysis.h"
 
 #ifdef __CINT__
 
-#pragma link off all class; 
-#pragma link off all function; 
-#pragma link off all global; 
+#pragma link off all class;
+#pragma link off all function;
+#pragma link off all global;
 #pragma link off all typedef;
 
 #pragma link C++ class DataEventSummary;
+#pragma link C++ class LxyTreeAnalysis;
 
 
 #endif
@@ -17,4 +19,3 @@
 // mode: sensitive
 // c-basic-offset: 8
 // End:
-

557124b46e90c7d3c63cb372b5c54044b526afd8
Benjamin Stieger <stiegerb@cern.ch>
2014-06-27 18:23:54 +0200

    Made slimAndSkim function exportable

--- scripts/treeSlimNSkim.py
+++ scripts/treeSlimNSkim.py
@@ -36,59 +36,36 @@ def in_file (option, opt_str, value, parser):
 def outputtime():
 	print "-"*5,round(time.time()-STARTTIME,3),time.asctime(),"-"*5
 
-def main(options, args):
-	files=[]
-
-	## Get input files
-	for arg in args:
-		if "," in arg:
-			files += arg.split(",")
-		else:
-			files.append(arg)
-
+def slimAndSkim(files, cut, in_ex_branch, output, treeloc='dataAnalyzer/lxy'):
 	print "-"*40
 	print "Using input files:"
 	print files
-	print "Writing to", options.output
+	print "Writing to", output
 
 	outputtime()
 
-	mainchain = ROOT.TChain(options.maintree)
-
-	print "chain", options.maintree
-	for f in files:
-		mainchain.Add(f)
-		print " ... adding file",f
+	## Get input files
+	mainchain = ROOT.TChain(treeloc)
+	print "chain", treeloc
+	for filename in files:
+		mainchain.Add(filename)
+		print " ... adding file",filename
 
 	outputtime()
 
-	print "copy chains"
-
-	copychains = []
-	chainbranches = []
-
-	for chain in options.copytree:
-		print "read",chain
-		copychain = ROOT.TChain(chain)
-		for f in files:
-			copychain.Add(f)
-		copychains.append(copychain)
-		if options.cut:
-			mainchain.AddFriend(copychain)
-			brlist = [ br.GetName() for br in copychain.GetListOfBranches() ]
-			print "add",chain,"and its branches",len(brlist)
-			chainbranches += brlist
-
-	if options.cut:
+	## Apply cut and get entrylist to loop over
+	if cut:
 		outputtime()
-		print " ... creating cut", options.cut
-		cut = ROOT.TCut(options.cut)
+		print " ... creating cut", cut
+		cut = ROOT.TCut(cut)
 		cutnumber = mainchain.Draw(">>cutlist",cut,"entrylist")
 		cutlist = ROOT.gDirectory.Get("cutlist")
 		mainchain.SetEntryList(cutlist)
 		print " Cut reduced number of events from", mainchain.GetEntries(),"to", cutnumber
 
 	outputtime()
+
+	## Get list of branches to activate
 	brancheslist = []
 	allbrancheslist = [br.GetName() for br in mainchain.GetListOfBranches()]
 
@@ -98,80 +75,78 @@ def main(options, args):
 	print " Starting with",len(brancheslist),"("+str(len(allbrancheslist))+")","branches"
 	for branch in in_ex_branch:
 		if branch[-1]=="+":
-			brancheslist+=[ b for b in allbrancheslist if re.match(branch[:-1],b)!=None ]
+			brancheslist += [b for b in allbrancheslist if re.match(branch[:-1],b)!=None]
 		if branch[-1]=="-":
-			brancheslist=[ b for b in brancheslist if re.match(branch[:-1],b)==None ]
+			brancheslist = [b for b in brancheslist if re.match(branch[:-1],b)==None ]
 		print " ... after",branch,"have",len(brancheslist),"branches"
 
-	if options.cut:
-		print " ... adding branches of other trees",len(chainbranches)
-		brancheslist+=chainbranches
-
 	outputtime()
-	print " ... activate branches",len(brancheslist)
-	mainchain.SetBranchStatus("*",0)
+
+	## Activate necessary branches
+	print " ... activate branches", len(brancheslist)
+	mainchain.SetBranchStatus("*", 0)
 	for branch in brancheslist:
 		mainchain.SetBranchStatus(branch,1)
 
 	outputtime()
-	print "new file",options.output
 
-	newfile = ROOT.TFile(options.output,"recreate")
+	## Create output file
+	print "new file",output
+	newfile = ROOT.TFile(output,"recreate")
 
-	print "copy main tree",options.maintree
+	## Copy the tree
+	print "copy main tree",treeloc
 	newtree = mainchain.CopyTree("")
-	if '/' in options.maintree:
-		newdir = newfile.mkdir(options.maintree.split('/')[0])
+
+	## Write out new tree
+	if '/' in treeloc:
+		newdir = newfile.mkdir(treeloc.split('/',1)[0])
 		newdir.cd()
-
 	newtree.Write()
-
-	if options.cut==None:
-		for chain in copychains:
-			print "copy tree",chain
-			newchain=chain.CopyTree("")
-			newchain.Write()
-
 	newfile.Close()
+
 	outputtime()
 	print "DONE"
 	print '-'*40
+
+def main(options, args):
+	## Get input files
+	files = []
+	for arg in args:
+		if "," in arg:
+			files += arg.split(",")
+		else:
+			files.append(arg)
+	slimAndSkim(files=files,
+	            cut=options.cut,
+	            in_ex_branch=in_ex_branch,
+	            output=options.output,
+	            treeloc=options.maintree)
+	exit(0)
+
 
 def addOptions(parser):
 	parser.add_option("-t", "--maintree", action="store",
 	                  type="str", dest="maintree", default="dataAnalyzer/lxy",
 	                  help=("tree name whose branches are removed and on "
                             "which the cuts are applied, default=%default"))
-
-	parser.add_option("--copytrees",
-	                  action="append", dest="copytree", default=[],
-	                  help=("trees that should also be copied to the output "
-	                        "file, default=%default"))
-
 	parser.add_option("-i", "--include", action="callback", type="str",
                       callback=include_list, help="branches to include")
-
 	parser.add_option("-e", "--exclude", action="callback", type="str",
                       callback=exclude_list, help="branches to exclude")
-
 	parser.add_option("-f", "--fileinclude", action="callback", type="str",
                       callback=include_file,
                       help="file with branch names to include")
-
-
 	parser.add_option("-F", "--fileexclude", action="callback", type="str",
                       callback=exclude_file,
                       help="file with branch names to exclude")
-
 	parser.add_option("-I", "--includefromfile", action="callback", type="str",
                       callback=in_file,
                       help=("file with branch name to in- and exclude, add + "
                             "or - at the end of the name"))
-
 	parser.add_option("-c", "--cut", action="store", type="str", dest="cut",
                       help="cut on tree")
-
-	parser.add_option("-o", "--ouput", action="store", type="str", dest="output",
+	parser.add_option("-o", "--output", action="store", type="str", dest="output",
 	                  default="output.root",
 	                  help="output filename, default=%default")
 

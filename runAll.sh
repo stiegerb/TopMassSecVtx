45a753ed45dcd959f4126b60d657725ad05203d1
Benjamin Stieger <stiegerb@cern.ch>
2015-02-18 16:24:50 +0100

    Updated runAll.sh, separate hash for data trees

--- runAll.sh
+++ runAll.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 WHAT=$1; if [[ "$1" == "" ]]; then echo "runAll.sh <TREES/ALLTREES/MERGE/PLOTS/FITS>"; exit 1; fi
+TAG=$2
 
-tag=Feb16
-treedir=/afs/cern.ch/work/s/stiegerb/TopSecVtx/SVLInfo/${tag}/
+treedir=/afs/cern.ch/work/s/stiegerb/TopSecVtx/SVLInfo/${TAG}/
 
 mkdir -p ${treedir}
 
+# hash="a176401"
 hash="e1fa735"
 
 echo "Running on "${treedir}
 
 case $WHAT in
 	TREES )
-		./scripts/runLxyTreeAnalysis.py -o ${treedir}            -j 8 /store/cmst3/group/top/summer2014/${hash}/
+		./scripts/runLxyTreeAnalysis.py -o ${treedir} -p MC8TeV   -j 8 /store/cmst3/group/top/summer2014/${hash}/
+		hash="a176401"
+		./scripts/runLxyTreeAnalysis.py -o ${treedir} -p Data8TeV -j 8 /store/cmst3/group/top/summer2014/${hash}/
+		;;
+	DATATREES )
+		hash="a176401"
+		./scripts/runLxyTreeAnalysis.py -o ${treedir} -p Data8TeV -j 8 /store/cmst3/group/top/summer2014/${hash}/
 		;;
 	ALLTREES )
 		./scripts/runLxyTreeAnalysis.py -o ${treedir}            -j 8 /store/cmst3/group/top/summer2014/${hash}/
@@ -25,10 +32,10 @@ case $WHAT in
 		./scripts/mergeSVLInfoFiles.py ${treedir}/syst/
 		;;
 	PLOTS )
-		./scripts/makeSVLDataMCPlots.py ${treedir}  -o svlplots/${tag}
-		./scripts/makeSVLControlPlots.py ${treedir} -o svlplots/${tag}
+		./scripts/makeSVLDataMCPlots.py ${treedir}  -o svlplots/${TAG}
+		./scripts/makeSVLControlPlots.py ${treedir} -o svlplots/${TAG}
 		;;
 	FITS )
-		./scripts/makeSVLWorkspace.py -i .svlhistos.pck -j 8 -o svlfits/${tag}
+		./scripts/makeSVLWorkspace.py -i .svlhistos.pck -j 8 -o svlfits/${TAG}
 		;;
 esac

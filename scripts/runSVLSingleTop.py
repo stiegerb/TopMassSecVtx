95a4fb214abc8e594b217eadb4f75f8fbaeba806
elizabeth drueke <elizabeth.drueke@cern.ch>
2015-06-29 16:17:50 +0200

    Updated Cut flow from last week, tmva analysis may work

--- scripts/runSVLSingleTop.py
+++ scripts/runSVLSingleTop.py
@@ -1,11 +1,232 @@
 #! /usr/bin/env python
 import os, sys
 import ROOT
+from ROOT import TMVA
 from copy import deepcopy
 from makeSVLDataMCPlots import resolveFilename
 from makeSVLMassHistos import LUMI
 from runPlotter import addPlotterOptions
 from UserCode.TopMassSecVtx.storeTools_cff import fillFromStore
+
+"""
+Attempt at implementing TMVA
+"""
+def runTMVAAnalysis(filenames,myMethodList=''):
+	
+	Use = {}
+
+	Use['Cuts']=1
+	Use['CutsD']=1
+	Use['CutsPCA']=0
+	Use['CutsGA']=0
+	Use['CutsSA']=0
+
+	Use['Likelihood']=1
+	Use['LikelihoodD']=0
+	Use['LikelihoodPCA']=1
+	Use['LikelihoodKDE']=0
+	Use['LikelihoodMIX']=0
+	
+	Use["PDERS"]           = 1
+	Use["PDERSD"]          = 0
+	Use["PDERSPCA"]        = 0
+	Use["PDEFoam"]         = 1
+	Use["PDEFoamBoost"]    = 0 
+	Use["KNN"]             = 1 
+	
+	Use["LD"]              = 1 
+	Use["Fisher"]          = 0
+	Use["FisherG"]         = 0
+	Use["BoostedFisher"]   = 0 
+	Use["HMatrix"]         = 0
+	
+	Use["FDA_GA"]          = 1 
+	Use["FDA_SA"]          = 0
+	Use["FDA_MC"]          = 0
+	Use["FDA_MT"]          = 0
+	Use["FDA_GAMT"]        = 0
+	Use["FDA_MCMT"]        = 0
+	
+	Use["MLP"]             = 0 
+	Use["MLPBFGS"]         = 0 
+	Use["MLPBNN"]          = 1 
+	Use["CFMlpANN"]        = 0 
+	Use["TMlpANN"]         = 0 
+	
+	Use["SVM"]             = 1
+	
+	Use["BDT"]             = 1 
+	Use["BDTG"]            = 0 
+	Use["BDTB"]            = 0 
+	Use["BDTD"]            = 0 
+	Use["BDTF"]            = 0  
+	
+	Use["RuleFit"]         = 1
+	
+	print('\n==> Start TMVA Classification\n')
+	
+	if myMethodList!='':
+		for key in Use.keys():
+			Use[key]=0
+		mlist = []
+		for key in Use.keys():
+			mlist.append(key)
+		i=0
+		while i<len(mlist):
+			regMethod = mlist[i]
+			if regMethod not in myMethodList.keys():
+				print('Method '+regMethod+' not known in TMVA under this name. Choose among the following: \n')
+				for key in Use.keys():
+					print(key+'\n')
+					return 0
+			Use[regMethod] = 1
+
+	outfileName = '/afs/cern.ch/user/e/edrueke/top_mass_lxy/CMSSW_5_3_22/src/UserCode/TopMassSecVtx/Cut_Analysis/TMVA_test_output.root'
+	outputFile = ROOT.TFile.Open(outfileName,'RECREATE')
+
+	factory = TMVA.Factory('TMVAClassification',outputFile,'!V:!Silent:Color:DrawProgressBar:Transformations=I;D;P;G,D:AnalysisType=Classification')
+
+	#Need to AddVariables to the factory here.
+
+	factory.AddVariable('NPVtx','NPVtx','Units','I')
+	factory.AddVariable('NJets','NJets','Units','I')
+	factory.AddVariable('NBTags','NBTags','Units','I')
+	factory.AddVariable('MET','MET','GeV','F')
+	factory.AddVariable('SVLDeltaR','SVLDeltaR','Units','F')
+	factory.AddVariable('LPt','LPt','GeV','F')
+	factory.AddVariable('SVMass','SVMass','GeV','F')
+	factory.AddVariable('SVNtrk','SVNtrk','Units','I')
+	factory.AddVariable('SVPt','SVPt','GeV','F')
+	factory.AddVariable('SVLxy','SVLxy','GeV','F')
+	factory.AddVariable('JPt','JPt','GeV','F')
+	factory.AddVariable('JEta','JEta','Units','F')
+	factory.AddVariable('FJPt','FJPt','GeV','F')
+	factory.AddVariable('FJEta','FJEta','Units','F')
+	factory.AddVariable('MT','MT','GeV','F')
+
+	sigNames = []
+	bkgNames = []
+	for item in filenames:
+		if 'SingleT' in item:
+			sigNames.append(item)
+		else:
+			bkgNames.append(item)
+
+	sigTrees = []
+	bkgTrees = []
+	for name in sigNames:
+		inFile = ROOT.TFile.Open(name)
+		myTree = inFile.Get('SVLInfo')
+		sigTrees.append(myTree)
+	for name in bkgNames:
+		inFile = ROOT.TFile.Open(name)
+		myTree = inFile.Get('SVLInfo')
+		bkgTrees.append(myTree)
+
+	#Need to figure out how to access the weights
+	weights = 1.0
+
+	for tree in sigTrees:
+		factory.AddSignalTree(tree,weights)
+	for tree in bkgTrees:
+		factory.AddBackgroundTree(tree,weights)
+
+	factory.SetBackgroundWeightExpression('1.0')
+
+	cutS = ROOT.TCut()
+	cutB = ROOT.TCut()
+	factory.PrepareTrainingAndTestTree(cutS,cutB,"nTrain_Signal=0:nTrain_Background=0:SplitMode=Random:NormMode=NumEvents:!V" )
+
+	if Use['Cuts']:
+		factory.BookMethod(TMVA.Types.kCuts,'Cuts','!H:!V:FitMethod=MC:EffSel:SampleSize=200000:VarProp=FSmart')
+	if Use['CutsD']:
+		factory.BookMethod(TMVA.Types.kCuts,'CutsD','!H:!V:FitMethod=MC:EffSel:SampleSize=200000:VarProp=FSmart:VarTransform=Decorrelate')
+	if Use['CutsPCA']:
+		factory.BookMethod(TMVA.Types.kCuts,'CutsPCA','!H:!V:FitMethod=MC:EffSel:SampleSize=200000:VarProp=FSmart:VarTransform=PCA')
+	if Use['CutsGA']:
+		factory.BookMethod(TMVA.Types.kCuts,'CutsGA','H:!V:FitMethod=GA:CutRangeMin[0]=-10:CutRangeMax[0]=10:VarProp[1]=FMax:EffSel:Steps=30:Cycles=3:PopSize=400:SC_steps=10:SC_rate=5:SC_factor=0.95')
+	if Use['CutsSA']:
+		factory.BookMethod(TMVA.Types.kCuts,'CutsSA','!H:!V:FitMethod=SA:EffSel:MaxCalls=150000:KernelTemp=IncAdaptive:InitialTemp=1e+6:MinTemp=1e-6:Eps=1e-10:UseDefaultScale')
+	if Use['Likelihood']:
+		factory.BookMethod(TMVA.Types.kLikelihood,'Likelihood','H:!V:TransformOutput:PDFInterpol=Spline2:NSmoothSig[0]=20:NSmoothBkg[0]=20:NSmoothBkg[1]=10:NSmooth=1:NAvEvtPerBin=50')
+		#factory.BookMethod(TMVA.Types.kLikelihood,'Likelihood','H:!V:TransformOutput:PDFInterpol=Spline2:NSmoothSig[0]=20:NSmoothBkg[0]=20:NSmooth=1:NAvEvtPerBin=50')
+	if Use['LikelihoodD']:
+		factory.BookMethod(TMVA.Types.kLikelihood,'LikelihoodD','!H:!V:TransformOutput:PDFInterpol=Spline2:NSmoothSig[0]=20:NSmoothBkg[0]=20:NSmooth=5:NAvEvtPerBin=50:VarTransform=Decorrelate')
+	if Use['LikelihoodPCA']:
+		factory.BookMethod(TMVA.Types.kLikelihood,'LikelihoodPCA','!H:!V:!TransformOutput:PDFInterpol=Spline2:NSmoothSig[0]=20:NSmoothBkg[0]=20:NSmooth=5:NAvEvtPerBin=50:VarTransform=PCA')
+	if Use['LikelihoodKDE']:
+		factory.BookMethod(TMVA.Types.kLikelihood,'LikelihoodKDE','!H:!V:!TransformOutput:PDFInterpol=KDE:KDEtype=Gauss:KDEiter=Adaptive:KDEFineFactor=0.3:KDEborder=None:NAvEvtPerBin=50')
+	if Use['LikelihoodMIX']:
+		factory.BookMethod(TMVA.Types.kLikelihood,'LikelihoodMIX','!H:!V:!TransformOutput:PDFInterpolSig[0]=KDE:PDFInterpolBkg[0]=KDE:PDFInterpolSig[1]=KDE:PDFInterpolBkg[1]=KDE:PDFInterpolSig[2]=Spline2:PDFInterpolBkg[2]=Spline2:PDFInterpolSig[3]=Spline2:PDFInterpolBkg[3]=Spline2:KDEtype=Gauss:KDEiter=Nonadaptive:KDEborder=None:NAvEvtPerBin=50')
+	if Use['PDERS']:
+		factory.BookMethod(TMVA.Types.kPDERS,'PDERS','!H:!V:NormTree=T:VolumeRangeMode=Adaptive:KernelEstimator=Gauss:GaussSigma=0.3:NEventsMin=400:NEventsMax=600')
+	if Use['PDERSD']:
+		factory.BookMethod(TMVA.Types.kPDERS,'PDERSD','!H:!V:VolumeRangeMode=Adaptive:KernelEstimator=Gauss:GaussSigma=0.3:NEventsMin=400:NEventsMax=600:VarTransform=Decorrelate')
+	if Use['PDERSPCA']:
+		factory.BookMethod(TMVA.Types.kPDERS,'PDERSPCA','!H:!V:VolumeRangeMode=Adaptive:KernelEstimator=Gauss:GaussSigma=0.3:NEventsMin=400:NEventsMax=600:VarTransform=PCA')
+	if Use['PDEFoam']:
+		factory.BookMethod(TMVA.Types.kPDEFoam,'PDEFoam','!H:!V:SigBgSeparate=F:TailCut=0.001:VolFrac=0.0666:nActiveCells=500:nSampl=2000:nBin=5:Nmin=100:Kernel=None:Compress=T')
+	if Use['PDEFoamBoost']:
+		factory.BookMethod(TMVA.Types.kPDEFoam,'PDEFoamBoost','!H:!V:Boost_Num=30:Boost_Transform=linear:SigBgSeparate=F:MaxDepth=4:UseYesNoCell=T:DTLogic=MisClassificationError:FillFoamWithOrigWeights=F:TailCut=0:nActiveCells=500:nBin=20:Nmin=400:Kernel=None:Compress=T')
+	if Use['KNN']:
+		factory.BookMethod(TMVA.Types.kKNN,'KNN','H:nkNN=20:ScaleFrac=0.8:SigmaFact=1.0:Kernel=Gaus:UseKernel=F:UseWeight=T:!Trim')
+	if Use['HMatrix']:
+		factory.BookMethod(TMVA.Types.kHMatrix,'HMatrix','!H:!V:VarTransform=None')
+	if Use['LD']:
+		factory.BookMethod(TMVA.Types.kLD,'LD','H:!V:VarTransform=None:CreateMVAPdfs:PDFInterpolMVAPdf=Spline2:NbinsMVAPdf=50:NsmoothMVAPdf=10')
+	if Use['Fisher']:
+		factory.BookMethod(TMVA.Types.kFisher,'Fisher','H:!V:Fisher:VarTransform=None:CreateMVAPdfs:PDFInterpolMVAPdf=Spline2:NbinsMVAPdf=50:NsmoothMVAPdf=10')
+	if Use['FisherG']:
+		factory.BookMethod(TMVA.Types.kFisher,'FisherG','H:!V:VarTransform=Gauss')
+	if Use['BoostedFisher']:
+		factory.BookMethod(TMVA.Types.kFisher,'BoostedFisher','H:!V:Boost_Num=20:Boost_Transform=log:Boost_Type=AdaBoost:Boost_AdaBoostBeta=0.2:!Boost_DetailedMonitoring')
+	if Use['FDA_MC']:
+		factory.BookMethod(TMVA.Types.kFDA,'FDA_MC','H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1);(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=MC:SampleSize=100000:Sigma=0.1')
+	if Use['FDA_GA']:
+		factory.BookMethod(TMVA.Types.kFDA,'FDA_GA','H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1);(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=GA:PopSize=300:Cycles=3:Steps=20:Trim=True:SaveBestGen=1')
+	if Use['FDA_SA']:
+		factory.BookMethod(TMVA.Types.kFDA,'FDA_SA','H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1);(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=SA:MaxCalls=15000:KernelTemp=IncAdaptive:InitialTemp=1e+6:MinTemp=1e-6:Eps=1e-10:UseDefaultScale')
+	if Use['FDA_MT']:
+		factory.BookMethod(TMVA.Types.kFDA,'FDA_MT','H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1);(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=MINUIT:ErrorLevel=1:PrintLevel=-1:FitStrategy=2:UseImprove:UseMinos:SetBatch')
+	if Use['FDA_GAMT']:
+		factory.BookMethod(TMVA.Types.kFDA,'FDA_GAMT','H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1);(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=GA:Converger=MINUIT:ErrorLevel=1:PrintLevel=-1:FitStrategy=0:!UseImprove:!UseMinos:SetBatch:Cycles=1:PopSize=5:Steps=5:Trim')
+	if Use['FDA_MCMT']:
+		factory.BookMethod(TMVA.Types.kFDA,'FDA_MCMT','H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1);(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=MC:Converger=MINUIT:ErrorLevel=1:PrintLevel=-1:FitStrategy=0:!UseImprove:!UseMinos:SetBatch:SampleSize=20')
+	if Use['MLP']:
+		factory.BookMethod(TMVA.Types.kMLP,'MLP','H:!V:NeuronType=tanh:VarTransform=N:NCycles=600:HiddenLayers=N+5:TestRate=5:!UseRegulator')
+	if Use['MLPBFGS']:
+		factory.BookMethod(TMVA.Types.kMLP,'MLPBFGS','H:!V:NeuronType=tanh:VarTransform=N:NCycles=600:HiddenLayers=N+5:TestRate=5:TrainingMethod=BFGS:!UseRegulator')
+	if Use['MLPBNN']:
+		factory.BookMethod(TMVA.Types.kMLP,'MLPBN','H:!V:NeuronType=tanh:VarTransform=N:NCycles=600:HiddenLayers=N+5:TestRate=5:TrainingMethod=BFGS:UseRegulator')
+	if Use['CFMlpANN']:
+		factory.BookMethod(TMVA.Types.kCFMlpANN,'CFMlpANN','!H:!V:NCycles=2000:HiddenLayers=N+1,N')
+	if Use['TMlpANN']:
+		factory.BookMethod(TMVA.Types.kTMlpANN,'TMlpANN','!H:!V:NCycles=200:HiddenLayers=N+1,N:LearningMethod=BFGS:ValidationFraction=0.3')
+	if Use['SVM']:
+		factory.BookMethod(TMVA.Types.kSVM,'SVM','Gamma=0.25:Tol=0.001:VarTransform=Norm')
+	if Use['BDTG']:
+		factory.BookMethod(TMVA.Types.kBDT,'BDTG','!H:!V:NTrees=1000:MinNodeSize=2.5%:BoostType=Grad:Shrinkage=0.10:UseBaggedBoost:BaggedSampleFraction=0.5:nCuts=20:MaxDepth=2')
+	if Use['BDT']:
+		factory.BookMethod(TMVA.Types.kBDT,'BDT','!H:!V:NTrees=850:SeparationType=GiniIndex:nCuts=20')
+		#factory.BookMethod(TMVA.Types.kBDT,'BDT','!H:!V:NTrees=850:MinNodeSize=2.5%:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:UseBaggedBoost:BaggedSampleFraction=0.5:SeparationType=GiniIndex:nCuts=20')
+	if Use['BDTB']:
+		factory.BookMethod(TMVA.Types.kBDT,'BDTB','!H:!V:NTrees=400:BoostType=Bagging:SeparationType=GiniIndex:nCuts=20')
+	if Use['BDTD']:
+		factory.BookMethod(TMVA.Types.kBDT,'BDTD','!H:!V:NTrees=400:MinNodeSize=5%:MaxDepth=3:BoostType=AdaBoost:SeparationType=GiniIndex:nCuts=20:VarTransform=Decorrelate')
+	if Use['BDTF']:
+		factory.BookMethod(TMVA.Types.kBDT,'BDTMitFisher','!H:!V:NTrees=50:MinNodeSize=2.5%:UseFisherCuts:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:SeparationType=GiniIndex:nCuts=20')
+	if Use['RuleFit']:
+		factory.BookMethod(TMVA.Types.kRuleFit,'RuleFit','H:!V:RuleFitModule=RFTMVA:Model=ModRuleLinear:MinImp=0.001:RuleMinDist=0.001:NTrees=20:fEventsMin=0.01:fEventsMax=0.5:GDTau=-1.0:GDTauPrec=0.01:GDStep=0.01:GDNSteps=10000:GDErrScale=1.02')
+
+	factory.TrainAllMethods()
+	factory.TestAllMethods()
+	factory.EvaluateAllMethods()
+
+	outputFile.Close()
+
+	print('\n==> Wrote root file: '+outputFile.GetName()+'\n')
+	print('==> TMVAClassification is done!\n')
+	print('Now run TMVAGui.\n')
 
 """
 single top selection
@@ -15,10 +236,11 @@ def runSingleTopAnalysis(filename,isData,outDir):
 	#prepare histograms to store
 	histos={}
 	for chCat in ['e','mu']:
-		for jetCat in ['1j','2j','3j','4j','5j']:
+		for jetCat in ['2j','3j']:
+			#for nTracks in ['1t','2t','3t']:
 			tag=chCat+jetCat
 			histos['NPVtx_'+tag]      = ROOT.TH1F('NPVtx_'+tag,';N_{PV}-N_{HP};Events',30,0,30)
-			histos['MT_'+tag]         = ROOT.TH1F('MT_'+tag,';Transverse mass [GeV];Events',50,50,250)
+			histos['MT_'+tag]         = ROOT.TH1F('MT_'+tag,';Transverse mass [GeV];Events',50,0,250)
 			histos['MET_'+tag]        = ROOT.TH1F('MET_'+tag,';Missing transverse energy [GeV];Events',50,0,200)
 			histos['FJPt_'+tag]       = ROOT.TH1F('FJPt_'+tag,';Transverse momentum [GeV];Events',10,30,230)
 			histos['FJEta_'+tag]      = ROOT.TH1F('FJEta_'+tag,';Pseudo-rapidity;Events',50,0,5)
@@ -62,83 +284,68 @@ def runSingleTopAnalysis(filename,isData,outDir):
 		weight = 1 if isData else SVLInfo.Weight[0]*SVLInfo.Weight[1]*SVLInfo.Weight[4]*SVLInfo.METWeight[0]*SVLInfo.BtagWeight[0]*SVLInfo.JESWeight[0]
 		lumiweight = 1 if isData else SVLInfo.XSWeight*LUMI
 
-		#EDIT
 		cnt_ini_events+=weight
 
 ######################################
 
-		#EDIT: Definitely keep exactly the same
 		#require e or mu events
 		if SVLInfo.EvCat!=-11 and SVLInfo.EvCat!=-13 : continue
 		chCat = 'e' if SVLInfo.EvCat==-11 else 'mu'
 
-		#EDIT
 		cnt_e_or_mu+=weight
 
 ####################################
 
-		#EDIT: Definitely keep this cut.  Check increase from 2.5 to 3.5
 		#require 1 forward jet
 		fwdeta=ROOT.TMath.Abs(SVLInfo.FJEta)
-		#if fwdeta<0 or fwdeta>5: continue
+		if fwdeta<3.0 or fwdeta>5: continue
 
-		#EDIT
 		cnt_1_fwd_jet+=weight
 
 ####################################
 
-		#EDIT: Definitely keep this cut.  Vary 2,3,4
 		#require at least 1 and less than 4 central jets
-		if SVLInfo.NJets<1 or SVLInfo.NJets>5 : continue		
+		if SVLInfo.NJets<1 or SVLInfo.NJets>3 : continue		
 		jetCat="%dj" % (SVLInfo.NJets)
-		if SVLInfo.NJets > 10: continue
 
-		#EDIT
-#		if(ROOT.TMath.Abs(SVLInfo.FJEta) < 2.5):
-#			print('Found a missing event')
-
-		#EDIT
 		cnt_central_jets+=weight
 
 ###################################
 
-		#EDIT: Definitely keep this cut exactly the same.
 		#require 1 SecVtx
-		#if SVLInfo.SVMass<=0 : continue
+		if SVLInfo.SVMass<=0 : continue
 
-		#EDIT
 		cnt_1_sec_vtx+=weight
 
 ###################################
 
-		#EDIT: Definitely keep this cut exactly the same.
 		#re-inforce the cut in MT
-		#if SVLInfo.MT<50 : continue
+		if SVLInfo.MT<50 : continue
 
-		#EDIT
 		cnt_MT_cut+=weight
 
 ###################################
 
-#EDIT
-		#Require 1 or 2 btagged jets
-		#if SVLInfo.NBTags!=1 and SVLInfo.NBTags!=2: continue
+		#require 1 or 2 btagged jets
+		if SVLInfo.NBTags!=1 and SVLInfo.NBTags!=2: continue
 
 ###################################
 
-#Edit
-		#Require SVMass > 0.5
-		#if SVLInfo.SVMass<0.5: continue
+		#require CJEta < 2
+		cjeta=ROOT.TMath.Abs(SVLInfo.JEta)
+		if 0 > cjeta or cjeta > 2.0: continue
 
 ###################################
 
-#EDIT
-		#Require CJEta < 2
-		#if SVLInfo.JEta > 2: continue
+		#separate into nTracks
+		#if SVLInfo.SVNtrk < 1 or SVLInfo.SVNtrk > 3: continue
+		#if SVLInfo.SVNtrk == 1:
+		#nTrack = '1t'
+		#elif SVLInfo.SVNtrk == 2:
+		#nTrack = '2t'
+		#else:
+		#nTrack = '3t'
 
-###################################
-
-		#EDIT
 		cnt_final_events+=weight
 
 		#EDIT: Commented out weight and moved up
@@ -147,7 +354,7 @@ def runSingleTopAnalysis(filename,isData,outDir):
 		#lumiweight = 1 if isData else SVLInfo.XSWeight*LUMI
 
 		#fill histograms with variables of interest
-		tag=chCat+jetCat
+		tag=chCat+jetCat#+'_'+nTrack
 		histos['NPVtx_'+tag]  .Fill(SVLInfo.NPVtx-1, weight)
 		histos['MT_'+tag]     .Fill(SVLInfo.MT,      weight)
 		histos['MET_'+tag]    .Fill(SVLInfo.MET,     weight)
@@ -157,7 +364,7 @@ def runSingleTopAnalysis(filename,isData,outDir):
 		histos['EtaJxEtaB_'+tag].Fill(SVLInfo.FJEta*SVLInfo.JEta, weight)
 		histos['SVLMass_'+tag].Fill(SVLInfo.SVLMass, weight)
 		histos['SVMass_'+tag] .Fill(SVLInfo.SVMass,  weight)
-		histos['CJEta_'+tag]  .Fill(ROOT.TMath.Abs(SVLInfo.JEta),    weight)
+		histos['CJEta_'+tag]  .Fill(cjeta,           weight)
 		histos['CJPt_'+tag]   .Fill(SVLInfo.JPt,     weight)
 		histos['NJets_'+tag]  .Fill(SVLInfo.NJets,   weight)
 		histos['NBTags_'+tag] .Fill(SVLInfo.NBTags,  weight)
@@ -232,15 +439,22 @@ def main(args, options):
 		print "Please provide a valid input directory"
 		return -1
 	
+	#EDIT
+	filenames = []
+
 	#submit tasks in parallel, if required, or run sequentially
-	if opt.jobs>0:
-		print ' Submitting jobs in %d threads' % opt.jobs
-		import multiprocessing as MP
-		pool = MP.Pool(opt.jobs)
-		pool.map(runSingleTopAnalysisPacked,taskList)
-	else:
-		for filename,isData,outDir in taskList:
-			runSingleTopAnalysis(filename=filename,isData=isData,outDir=outDir)
+#	if opt.jobs>0:
+#		print ' Submitting jobs in %d threads' % opt.jobs
+#		import multiprocessing as MP
+#		pool = MP.Pool(opt.jobs)
+#		pool.map(runSingleTopAnalysisPacked,taskList)
+#	else:
+#		for filename,isData,outDir in taskList:
+#			runSingleTopAnalysis(filename=filename,isData=isData,outDir=outDir)
+	#EDIT
+	for filename,isData,outDir in taskList:
+		filenames.append(filename)
+	runTMVAAnalysis(filenames)
 
 	#EDIT: Write current cuts to sig/bkg file
 	sigbkg = open('sig_bkg.txt','a')

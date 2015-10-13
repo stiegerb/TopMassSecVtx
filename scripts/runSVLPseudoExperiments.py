45ecac3dea6d6739264ca5e04e428e470854f119
Pedro Silva <psilva@cern.ch>
2015-10-13 09:45:44 +0200

    updating unblinding in the proper place

--- scripts/runSVLPseudoExperiments.py
+++ scripts/runSVLPseudoExperiments.py
@@ -662,9 +662,12 @@ def main():
 
         return 0
     else:
-        print 'Ready to unblind?'
-        print '...ah ah this is not even implemented'
+	opt.isData=True
+        opt.spy=True
+        opt.nPexp=1
+        runPseudoExperiments(ws,options=opt,experimentTag='data')
         return -1
+
     print 80*'-'
     return 0
 

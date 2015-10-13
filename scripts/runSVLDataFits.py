aa960900afebe12b0eafb94a9ae49b1c743086fb
Benjamin Stieger <stiegerb@cern.ch>
2015-10-13 17:00:26 +0200

    Added script for unblinded fits

---
+++ scripts/runSVLDataFits.py
@@ -0,0 +1,298 @@
+#!/usr/bin/env python
+import ROOT
+import os,sys
+import os.path as osp
+import optparse
+import pickle
+import numpy
+
+from UserCode.TopMassSecVtx.PlotUtils import printProgress, bcolors
+from makeSVLMassHistos import NTRKBINS
+from summarizeSVLresults import CATTOLABEL
+from myRootFunctions import checkKeyInFile
+
+from runSVLPseudoExperiments import PseudoExperimentResults, showFinalFitResult
+
+
+"""
+run the fits on the data
+"""
+def runDataFits(wsfile,pefile,options):
+    prepend = '[runDataFits] '
+
+    #read the file with input distributions
+    inputDistsF = ROOT.TFile.Open(pefile, 'READ')
+    print prepend+'Reading data histos from %s with' % (pefile)
+    if not checkKeyInFile('data', inputDistsF, doraise=False):
+        print prepend+"ERROR: data histos not found in input file %s" %(pefile)
+        sys.exit(-1)
+
+    wsInputFile = ROOT.TFile.Open(wsfile, 'READ')
+    ws = wsInputFile.Get('w')
+    wsInputFile.Close()
+    print prepend+'Read workspace from %s' % wsfile
+
+    #readout calibration from a file
+    calibMap=None
+    if options.calib:
+         cachefile = open(options.calib,'r')
+         calibMap  = pickle.load(cachefile)
+         cachefile.close()
+         print prepend+'Read calibration from %s'%options.calib
+
+    genMtop=172.5
+    print prepend+'Assuming top mass is %5.1f GeV'%genMtop
+
+    #prepare results summary
+    selTag=''
+    if len(options.selection)>0 : selTag='_%s'%options.selection
+    summary=PseudoExperimentResults(genMtop=genMtop,
+                                    outFileUrl=osp.join(options.outDir,'data%s_results.root'%(selTag)),
+                                    selection=options.selection)
+
+    #load the model parameters and set all to constant
+    ws.loadSnapshot("model_params")
+    allVars = ws.allVars()
+    varIter = allVars.createIterator()
+    var = varIter.Next()
+    varCtr=0
+    while var :
+        varName=var.GetName()
+        if not varName in ['mtop', 'SVLMass', 'mu']:
+        # if not varName in ['mtop', 'SVLMass']:
+            ws.var(varName).setConstant(True)
+            varCtr+=1
+        var = varIter.Next()
+    print prepend+'setting to constant %d numbers in the model'%varCtr
+
+    ws.var("mtop").setVal(genMtop)
+    ws.var("mu").setVal(1.0)
+
+    #build the relevant PDFs
+    allPdfs = {}
+    for ch in ['em','mm','ee','m','e']:
+        chsel=ch
+        if len(options.selection)>0 : chsel += '_' + options.selection
+        for ntrk in [tklow for tklow,_ in NTRKBINS]: # [3,4,5]
+            ttexp      = '%s_ttexp_%d'              %(chsel,ntrk)
+            ttcor      = '%s_ttcor_%d'              %(chsel,ntrk)
+            ttcorPDF   = 'simplemodel_%s_%d_cor_tt' %(chsel,ntrk)
+            ttwro      = '%s_ttwro_%d'              %(chsel,ntrk)
+            ttwroPDF   = 'simplemodel_%s_%d_wro_tt' %(chsel,ntrk)
+            ttunmPDF   = 'model_%s_%d_unm_tt'       %(chsel,ntrk)
+            tfrac      = '%s_tfrac_%d'              %(chsel,ntrk)
+            tcor       = '%s_tcor_%d'               %(chsel,ntrk)
+            tcorPDF    = 'simplemodel_%s_%d_cor_t'  %(chsel,ntrk)
+            twrounmPDF = 'model_%s_%d_wrounm_t'     %(chsel,ntrk)
+            bkgExp     = '%s_bgexp_%d'              %(chsel,ntrk)
+            bkgPDF     = 'model_%s_%d_unm_bg'       %(chsel,ntrk)
+
+            ## Check for existence of necessary pdfs in Workspace
+            for key in [ttcorPDF, ttwroPDF, ttunmPDF, tcorPDF, twrounmPDF, bkgPDF]:
+                if not ws.pdf(key):
+                    print "ERROR: pdf %s not found in workspace!" %key
+                    sys.exit(-1)
+
+            ttShapePDF = ws.factory("SUM::ttshape_%s_%d(%s*%s,%s*%s,%s)"%(
+                                           chsel,ntrk,ttcor,ttcorPDF,ttwro,ttwroPDF,ttunmPDF))
+            Ntt        = ws.factory("RooFormulaVar::Ntt_%s_%d('@0*@1',{mu,%s})"%(chsel,ntrk,ttexp))
+            # Ntt is fixed at the expected events for 172.5 GeV
+
+            tShapePDF  = ws.factory("SUM::tshape_%s_%d(%s*%s,%s)"%(
+                                           chsel,ntrk,tcor,tcorPDF,twrounmPDF))
+            Nt         = ws.factory("RooFormulaVar::Nt_%s_%d('@0*@1*@2',{mu,%s,%s})"%(
+                                           chsel,ntrk,ttexp,tfrac))
+
+            bkgConstPDF = ws.factory('Gaussian::bgprior_%s_%d('
+                                             'bg0_%s_%d[0,-10,10],'
+                                             'bg_nuis_%s_%d[0,-10,10],1.0)'%(
+                                             chsel,ntrk,chsel,ntrk,chsel,ntrk))
+            ws.var('bg0_%s_%d'%(chsel,ntrk)).setVal(0.0)
+            ws.var('bg0_%s_%d'%(chsel,ntrk)).setConstant(True)
+            #ws.var('bg_nuis_%s_%d'%(chsel,ntrk)).setVal(0.0)
+            #ws.var('bg_nuis_%s_%d'%(chsel,ntrk)).setConstant(True)
+
+            #30% unc on background
+            Nbkg = ws.factory("RooFormulaVar::Nbkg_%s_%d('@0*max(1+0.30*@1,0.)',"
+                                            "{%s,bg_nuis_%s_%d})"%(
+                                             chsel,ntrk,bkgExp,chsel,ntrk))
+
+            print '[Prefit yields] %12s, %d: %8.2f (Ntt: %8.2f) (Nt: %8.2f) (Bkg: %8.2f)' % (
+                                  chsel, ntrk, Ntt.getVal()+Nt.getVal()+Nbkg.getVal(),
+                                  Ntt.getVal(), Nt.getVal(), Nbkg.getVal())
+
+            summary.incrementPreFitExpectation(Ntt.getVal(), Nt.getVal(), Nbkg.getVal())
+
+            #see syntax here https://root.cern.ch/root/html/RooFactoryWSTool.html#RooFactoryWSTool:process
+            sumPDF = ws.factory("SUM::uncalibexpmodel_%s_%d( %s*%s, %s*%s, %s*%s )"%(chsel,ntrk,
+                                                    Ntt.GetName(), ttShapePDF.GetName(),
+                                                    Nt.GetName(), tShapePDF.GetName(),
+                                                    Nbkg.GetName(), bkgPDF))
+            ws.factory('PROD::uncalibmodel_%s_%d(%s,%s)'%(chsel,ntrk,
+                                                          sumPDF.GetName(),
+                                                          bkgConstPDF.GetName()))
+
+            #add calibration for this category if available (read from a pickle file)
+            offset, slope = 0.0, 1.0
+            if calibMap:
+                try:
+                    offset, slope = calibMap[options.selection][ '%s_%d'%(ch,ntrk) ]
+                except KeyError, e:
+                    print 'Failed to retrieve calibration with',e
+            ws.factory("RooFormulaVar::calibmtop_%s_%d('(@0-%f)/%f',{mtop})"%(chsel,ntrk,offset,slope))
+            allPdfs[(chsel,ntrk)] = ws.factory("EDIT::model_%s_%d(uncalibmodel_%s_%d,mtop=calibmtop_%s_%d)"%
+                                               (chsel,ntrk,chsel,ntrk,chsel,ntrk))
+
+
+    poi = ROOT.RooArgSet( ws.var('mtop') )
+
+    # Iterate over available categories to build the set of likelihoods to combine
+    nllMap={}
+
+    for key in sorted(allPdfs):
+        chsel, trk = key
+        mukey=(chsel+'_mu',trk)
+
+        if options.verbose>3:
+            sys.stdout.write(prepend+'(%-12s, %d):' % (chsel, trk))
+            sys.stdout.flush()
+
+        ws.var('mtop').setVal(172.5)
+        ws.var('mu').setVal(1.0)
+
+        #read histogram and generate random data
+        ihist = inputDistsF.Get('data/SVLMass_%s_data_%d'%(chsel,trk))
+
+        dataHisto, data = None,None
+        dataHisto = ihist.Clone('eh')
+        data  = ROOT.RooDataHist('Data_data_%s_%d'%(chsel,trk),
+                                       'Data_data_%s_%d'%(chsel,trk),
+                                        ROOT.RooArgList(ws.var('SVLMass')),
+                                        dataHisto)
+        # Create likelihood
+        # Store it in the appropriate categories for posterior combination
+        for nllMapKey in [('comb',0),('comb',trk),('comb%s'%chsel,0)]:
+            if not (nllMapKey in nllMap):
+                nllMap[nllMapKey]=[]
+            if nllMapKey[0]=='comb' and nllMapKey[1]==0:
+                nllMap[nllMapKey].append( allPdfs[key].createNLL(data, ROOT.RooFit.Extended()) )
+            else:
+                nllMap[nllMapKey].append( nllMap[('comb',0)][-1] )
+
+        if options.verbose>3:
+            sys.stdout.write(' [running Minuit]')
+            sys.stdout.flush()
+        minuit=ROOT.RooMinuit(nllMap[('comb',0)][-1])
+        minuit.setErrorLevel(0.5)
+        minuit.migrad()
+        minuit.hesse()
+        minuit.minos(poi)
+
+        #save fit results
+        summary.addFitResult(key=key, ws=ws)
+
+        #show, if required
+        selstring = options.selection if options.selection else 'inclusive'
+        pll=nllMap[('comb',0)][-1].createProfile(poi)
+
+        showFinalFitResult(data=data,pdf=allPdfs[key], nll=[pll,nllMap[('comb',0)][-1]],
+                           SVLMass=ws.var('SVLMass'),mtop=ws.var('mtop'),
+                           outDir=options.outDir,
+                           tag=[selstring,
+                           "%s channel, =%s tracks"%(
+                             str(chsel.split('_',1)[0]),
+                             str(trk))])
+
+        if options.verbose>3:
+            sys.stdout.write('%s DONE %s'
+                             '(mt: %6.2f+-%4.2f GeV, '
+                              'mu: %4.2f+-%4.2f)\n'%
+                            (bcolors.OKGREEN,bcolors.ENDC,
+                             ws.var('mtop').getVal(), ws.var('mtop').getError(),
+                             ws.var('mu').getVal(), ws.var('mu').getError()) )
+            sys.stdout.flush()
+
+    #combined likelihoods
+    if options.verbose>3:
+        print '%s------ Combining channels and categories'%(prepend)
+
+    for key in sorted(nllMap.keys()):
+
+        #reset to central values
+        ws.var('mtop').setVal(172.5)
+        ws.var('mu').setVal(1.0)
+
+        #add the log likelihoods and minimize
+        llSet = ROOT.RooArgSet()
+        for ll in nllMap[key]: llSet.add(ll)
+        combll = ROOT.RooAddition("combll","combll",llSet)
+        minuit=ROOT.RooMinuit(combll)
+        minuit.setErrorLevel(0.5)
+        minuit.migrad()
+        minuit.hesse()
+        minuit.minos(poi)
+        summary.addFitResult(key=key,ws=ws)
+
+        combll.Delete()
+
+        if options.verbose>3:
+            try: catlabel = CATTOLABEL[('%s_%d'%key)]
+            except KeyError: catlabel = CATTOLABEL[('%s_%d'%key).replace('_%s'%options.selection, '')]
+            sys.stdout.write(prepend)
+            sys.stdout.write('%8s (%2d cats): ' % (catlabel,len(nllMap[key])))
+            resultstring = ('mt: %6.2f+-%4.2f GeV, mu: %5.3f+-%5.3f \n' % (
+                               ws.var('mtop').getVal(), ws.var('mtop').getError(),
+                               ws.var('mu').getVal(), ws.var('mu').getError()) )
+            if key == ('comb', 0):
+                resultstring = bcolors.BOLD + resultstring + bcolors.ENDC
+            sys.stdout.write(resultstring)
+            sys.stdout.flush()
+    print 80*'-'
+
+    summary.saveResults()
+    return 0
+
+"""
+steer
+"""
+def main():
+    usage = """
+    Run the mass fit on the data distributions:
+    usage: %prog [options] SVLWorkspace.root pe_inputs.root
+    """
+    parser = optparse.OptionParser(usage)
+    parser.add_option('-v', '--verbose', dest='verbose', default=0, type=int,
+                       help='Verbose mode')
+    parser.add_option('-s', '--selection', dest='selection', default='',
+                       help='selection type')
+    parser.add_option('-c', '--calib', dest='calib', default='',
+                       help='calibration file')
+    parser.add_option('-o', '--outDir', dest='outDir', default='svlfits/pexp',
+                       help='Output directory [default: %default]')
+
+    (opt, args) = parser.parse_args()
+
+    ROOT.gStyle.SetOptStat(0)
+    ROOT.gStyle.SetOptTitle(0)
+    ROOT.gROOT.SetBatch(True)
+    ROOT.gSystem.Load("libUserCodeTopMassSecVtx")
+    ROOT.AutoLibraryLoader.enable()
+    if not opt.verbose > 5:
+        ROOT.shushRooFit()
+    # see TError.h - gamma function prints lots of errors when scanning
+    ROOT.gROOT.ProcessLine("gErrorIgnoreLevel=kFatal")
+
+    print 'Storing output in %s' % opt.outDir
+    os.system('mkdir -p %s' % opt.outDir)
+    os.system('mkdir -p %s' % osp.join(opt.outDir, 'plots'))
+
+    opt.verbose = 5
+    runDataFits(wsfile=args[0], pefile=args[1], options=opt)
+
+    print 80*'-'
+    return 0
+
+
+
+if __name__ == "__main__":
+    sys.exit(main())
